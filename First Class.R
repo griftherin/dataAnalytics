5b03a02941a67825b96f0254b1b8b613f6cec09b
Mubaarak Abdulhameed <53459467+griftherin@users.noreply.github.com>
2022-03-10 04:26:21 +0100

    Add files via upload

---
+++ First Class.R
@@ -0,0 +1,15 @@
+a = 5 #this is how to assign a scalar value
+b = 19
+c = b%%a
+print(c)
+
+age = c(10,15,20) #this is how to assign a vector variable
+mean(age)
+sd(age)
+sqrt(age)
+sqrt(b)
+a^4
+age * 2
+x = 1:10
+print(x)
+y = 1:10
